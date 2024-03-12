47bdc01afdca49a40cc2b316cea8bcb40d57dee0
alegreco7 <alexgreco2002@gmail.com>
2024-03-12 12:35:02 +0100

    all routes added

---
+++ src/router.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from "vue-router";
+import AppHome from './pages/AppHome.vue'
+import AppProjects from './pages/AppProjects.vue'
+import SingleProject from './pages/AppSingleProject.vue'
+import AppThanks from './pages/AppThanks.vue'
+import AppContact from './pages/AppContact.vue'
+import AppNotFound from './pages/AppNotFound.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+
+    routes: [
+        { path: '/', name: 'home', component: AppHome },
+        { path: '/projects', name: 'projects', component: AppProjects },
+        { path: '/projects/:slug', name: 'single-project', component: SingleProject },
+        { path: '/contacts', name: 'contacts', component: AppContact },
+        { path: '/thank', name: 'ThankYou', component: AppThanks },
+        { path: '/:pathMatch(.*)*', name: 'NotFound', component: AppNotFound }
+    ]
+})
+
+export { router };
