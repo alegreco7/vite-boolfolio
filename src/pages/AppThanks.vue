311e788a3d0b6ad7ba90edc473e8d65e7ddd6493
alegreco7 <alexgreco2002@gmail.com>
2024-03-13 09:14:21 +0100

    page thanks

---
+++ src/pages/AppThanks.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    
+}
+</script>
+<template lang="">
+    <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-6">
+                <h3>Email inviata con successo</h3>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="">
+    
+</style>
