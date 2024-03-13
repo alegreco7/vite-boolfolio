df6e7bad534e2a4e794f99db42736ca8a54fde12
alegreco7 <alexgreco2002@gmail.com>
2024-03-13 09:13:18 +0100

    page home

---
+++ src/pages/AppHome.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: 'AppHome'
+}
+</script>
+<template lang="">
+    <div class="container py-5">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <div class="row my-5">
+                    <div class="col-7">
+                        <h2>Progetti</h2>
+                            <ul>
+                                <li><strong>Nome:</strong>  Il titolo del progetto.</li>
+                                <li><strong>Descrizione:</strong> Una breve panoramica del progetto, compresi gli obiettivi e i risultati.</li>
+                                <li><strong>Tipo:</strong> Il tipo di progetto (ad esempio, sito web, applicazione web, ecc.).</li>
+                                <li><strong>Tecnologie usate:</strong>  Un elenco delle tecnologie e degli strumenti che ho utilizzato per realizzare il progetto.</li>
+                                <li><strong>Data di Creazione:</strong> La data in cui ho iniziato a lavorare sul progetto.</li>
+                                <li><strong>Data di Termine:</strong> La data in cui ho completato il progetto.</li>
+                            </ul>
+                    </div>
+                    
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+    @use '../styles/generals.scss';
+
+</style>
